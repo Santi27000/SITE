b5d30c19a3224f3693b3f0ed0c496c09845cca5b
Santi27000 <leandro.cruz.rosa@escola.pr.gov.br>
2025-07-29 09:27:24 -0300

    Update curriculo.php

--- Controll/curriculo.php
+++ Controll/curriculo.php
@@ -1,1 +1,50 @@
+<?php
+$nome = $_post["nome"];
+$telefone = $_post["telefone"];
+$email = $_post["email"];
+$endereco = $_post["endereco"];
+$efc = $_post["efc"];
+$emc = $_post["emc"];
+$esc = $_post["esc"
+$pgc = $_post["pgc"];
+$mc = $_post["mc"];
 
+$empresa = $_post["empresa"];
+$data_admissão = $_post["data_adimissão"];
+$data_demissão = $_post["data_demissão"];
+$atividade_exercida = $_post["atividade_exercida"];
+
+$descricao = $_post["descriçao"];
+$horas = $_post["horas"];
+$ano_conclusão = $_post["ano_conclusão"];
+
+$informaçoes = $_post["informacoes"];
+
+echo " Dados Pessoais"
+echo $nome,
+echo $telefone,
+echo $endereco,
+echo "Escolaridade";
+echo $efc,
+echo $emc,
+echo $esc,
+echo $pgc,
+echo $mc,
+
+echo "experiencia profissional";
+echo $empresa;
+echo $data_admissao;
+echo $data_demissao;
+echo $atividade_exercida;
+echo "curso e idiomas";
+echo $descricao;
+echo $horas;
+echo $ano_conclusao;
+echo $i"nformacoes_adicionais";
+echo $informacoes;
+
+
+
+
+
+?>
